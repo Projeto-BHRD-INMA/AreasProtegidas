161a51f21f1bed724f2db55fff90c54714050b59
Talita Zupo <talita.zupo@gmail.com>
2020-04-21 09:37:32 -0300

    plots using ggplot2

--- R Scripts/2-plotting.R
+++ R Scripts/2-plotting.R
@@ -38,9 +38,62 @@ plot(mun2, add = TRUE, col = 'green', axes = TRUE)
 
 #o arquivo uc_all foi tb baixado do MMA e, em teoria, era pra ser a soma das UCs
 #mas ao plotar, percebemos que sao diferentes. e agora?
+
 plot(bhrd)
 plot(all, add = TRUE,  col = 'orange', axes = TRUE)
 
 plot(munic)
 plot(all, add = TRUE,  col = 'orange', axes = TRUE)
 
+#### vamos tentar unir todas as UCs
+
+union_fed <- union(fed1, fed2)
+union_est <- union(est1, est2)
+union_mun <- union(mun1, mun2)
+
+#passos para conseguir usar o ggplot. shps need to be transformed for presentation by ggplot2.
+
+union_fed@data$id = rownames(union_fed@data)
+fed.points = fortify(union_fed, region="id")
+fed.df = join(fed.points, union_fed@data, by="id")
+
+union_est@data$id = rownames(union_est@data)
+est.points = fortify(union_est, region="id")
+est.df = join(est.points, union_est@data, by="id")
+
+union_mun@data$id = rownames(union_mun@data)
+mun.points = fortify(union_mun, region="id")
+mun.df = join(mun.points, union_mun@data, by="id")
+
+bhrd@data$id = rownames(bhrd@data)
+bhrd.points = fortify(bhrd, region="id")
+bhrd.df = join(fed.points, bhrd@data, by="id")
+
+library(plyr)
+library(ggplot2)
+library(gridExtra)
+library(maptools)
+
+
+plot1 <- ggplot() +
+  geom_polygon(data = bhrd, aes(long, lat, group = group), fill = 'grey90') +
+  geom_polygon(data = fed.points, aes(long, lat, group = group),
+               fill = "grey50",
+               alpha = 0.7) +
+  geom_polygon(data = est.points, aes(long, lat, group = group),
+               fill = "#003300",
+               alpha = 0.7) +
+  geom_polygon(data = mun.points, aes(long, lat, group = group),
+               fill = "black",
+               alpha = 0.7) +
+    theme_void()
+
+png("figs/figura1.png", res = 300, width = 1800, height = 1000)
+grid.arrange(plot1)
+dev.off()
+
+#+
+ # annotate(geom = "text", x = -60, y = -15, label = "South \n America",
+           #color = "grey30", size = 3) #+
+#annotate(geom = "text", x = -59, y = -7, label = "Brazilian Amazon",
+#    color = "grey90", size = 1.5)
